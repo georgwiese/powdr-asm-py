686521c1b24fcc67852aaada5d370fc06931dd4f
Georg Wiese <georgwiese@gmail.com>
2024-05-26 02:11:14 +0200

    Add comments

--- hello_world.asm
+++ hello_world.asm
@@ -10,11 +10,17 @@ A <== 0;
 // B stores the product
 B <== 1;
 
+// If index is 10, skip to the final assertion
 branch_if_zero(A - 10, 11);
+// Load the value at index A
 C <== mload(A);
+// Skip the next instruction if C is 0
 branch_if_zero(C, 9);
+// Multiply C into the accumulator
 B <== mul(B, C);
+// Increment the index
 A <== A + 1;
+// Jump back to the beginning of the loop
 jump(5);
 
 // The result should be 8
